6a42aa83e8ad3d2d04d4e84b6f380310fc3e6bd5
Matias <matiaspierpaoli@gmail.com>
2022-04-26 11:50:44 -0300

    Añadidos los prefabs y ubicados

---
+++ Memotest/Assets/Scripts/MoveCard.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCard : MonoBehaviour
+{
+    private void OnMouseDown()
+    {
+        transform.Rotate(180, 0, 0);
+        Debug.Log("Hola");
+    }
+}

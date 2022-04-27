593bc08c4fe109a89dfc32593e0674c853b446a8
Matias <matiaspierpaoli@gmail.com>
2022-04-27 13:23:20 -0300

    Cambiados los tags y la estructura de conexion entre gameobjects

--- Memotest/Assets/Scripts/CardsCreation.cs
+++ Memotest/Assets/Scripts/CardsCreation.cs
@@ -4,17 +4,21 @@ using UnityEngine;
 
 public class CardsCreation : MonoBehaviour
 {
+
+    //public bool activated = true;
+    //public bool isActivated() { return activated; }
+    //public void deactivate() { activated = false; }
+
+    //public static GameObject[] cards;
+
     
-    public bool activated = true;
-    public bool isActivated() { return activated; }
-    public void deactivate() { activated = false; }
-        
-    public static List<GameObject> cards;
+
+    public static List<GameObject> cards = new List<GameObject>();
 
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
--- Memotest/Assets/Scripts/MoveCard.cs
+++ Memotest/Assets/Scripts/MoveCard.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class MoveCard : MonoBehaviour
 {
+    public static GameObject[] gameObjects = null;
     private void Start()
     {
         
@@ -12,20 +13,63 @@ public class MoveCard : MonoBehaviour
 
     private void OnMouseDown()
     {
-        CardsCreation.cards.Add(GetComponent<GameObject>());
-
-        //CardsCreation.cards.deactivate();
-
-        for (var i = 0; i < CardsCreation.cards.Count; i++)
+        if (CardsCreation.cards.Count == 0)
         {
-            if (CardsCreation.cards[i])
-            {
-
-            }
+            gameObjects[0] = GetComponent<GameObject>();
+            CardsCreation.cards.Add(gameObjects[0]);
+            Debug.Log("Se aniadio el primer componente a la lista");           
+            transform.Rotate(180, 0, 0);
+        }
+        else if (CardsCreation.cards.Count > 0 && CardsCreation.cards.Count < 2)
+        {
+            gameObjects[1] = GetComponent<GameObject>();
+            CardsCreation.cards.Add(gameObjects[1]);
+            Debug.Log("Se aniadio el segundo componente a la lista");          
+            transform.Rotate(180, 0, 0);
         }
 
-       
-        transform.Rotate(180, 0, 0);      
+        if (CardsCreation.cards.Count == 2)
+        {
+            Debug.Log("Hay dos componentes en la lista");
 
+            if (gameObjects[0].tag == "Whale" && gameObjects[1].tag == "Whale")
+            {
+                Debug.Log("Ambos gameObjects eran Whale");
+                gameObjects[1].SetActive(false);
+                gameObjects[1].SetActive(false);
+            }
+            else if (gameObjects[0].tag == "Lion" && gameObjects[1].tag == "Lion")
+            {
+                Debug.Log("Ambos gameObjects eran Lion");
+                gameObjects[1].SetActive(false);
+                gameObjects[1].SetActive(false);
+            }
+            else if (gameObjects[0].tag == "Dolphin" && gameObjects[1].tag == "Dolphin")
+            {
+                Debug.Log("Ambos gameObjects eran Dolphin");
+                gameObjects[1].SetActive(false);
+                gameObjects[1].SetActive(false);
+            }
+            else if (gameObjects[0].tag == "Horse" && gameObjects[1].tag == "Horse")
+            {
+                Debug.Log("Ambos gameObjects eran Horse");
+                gameObjects[1].SetActive(false);
+                gameObjects[1].SetActive(false);
+            }
+            else if (gameObjects[0].tag == "Giraffe" && gameObjects[1].tag == "Giraffe")
+            {
+                Debug.Log("Ambos gameObjects eran Giraffe");
+                gameObjects[1].SetActive(false);
+                gameObjects[1].SetActive(false);
+            }
+            else
+            {
+                gameObjects[0].transform.Rotate(180, 0, 0);
+                 gameObjects[1].transform.Rotate(180, 0, 0);
+            }
+
+            CardsCreation.cards.Clear();
+            Debug.Log("La lista quedo vacia");
+        }
     }
 }
